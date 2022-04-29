a8209947b955a283747017813812912665046090
your_github_username <your_email@github.com>
2022-04-29 19:30:28 -0400

    project

--- dark_and_stormy.R
+++ dark_and_stormy.R
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-print("It was a dark and stormy night")
-=======
 print("It was a dark and stormy night so I stayed in to complete my R project. (Helped me avoid Covid-19 too)")
-
-
->>>>>>> 566d9b6ba26da6edab04fc46a4f72e4237e440ca
